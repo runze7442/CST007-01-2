554c7f641367635e3a6bcf0483623581864f80b4
Li Runze <lrz_ms@gmail.com>
2024-10-30 02:01:49 +0800

    测试

---
+++ icourse_backend/src/com/icourse/test/TestJwtHelper.java
@@ -0,0 +1,30 @@
+package com.icourse.test;
+
+import com.icourse.util.JwtHelper;
+import org.testng.annotations.Test;
+
+public class TestJwtHelper {
+
+    @Test
+    public void testAllMethod() throws InterruptedException {
+        String token = JwtHelper.createToken(1L);
+
+        System.out.println(token);
+
+        Long userId = JwtHelper.getUserId(token);
+        System.out.println(userId);
+
+
+        System.out.println(JwtHelper.isExpiration(token));
+
+
+        Thread.sleep(6000);
+
+        System.out.println(JwtHelper.isExpiration(token));
+
+
+
+
+    }
+
+}
